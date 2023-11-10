efb355c3d94eba7a02ba8c61ef5627cac82eb719
vitaliit-vvd <vitalii.t@vevidi.com>
2023-11-10 23:13:55 +0200

    add inline attribute edit

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class Customer < ApplicationRecord
+  EDITABLE_ATTRS = %i[first_name last_name date_of_birth phone description].freeze
+
+  validates :first_name, presence: true
 end
